8d0828cf013396b398412456d0a74ff8620b576b
Keegan Wilson <keegan.wilson@gmail.com>
2022-04-01 12:00:18 -0600

    Completed orders section

--- orders.js
+++ orders.js
@@ -30,7 +30,19 @@
 */
 
 //CODE HERE
+class Ticket {
+    status = 'queued'  
+    constructor(items, orderTime, customerId) {
+        this.items = items;
+        this.orderTime = orderTime;
+        this.customerId = customerId;
+        }
 
+    updateStatus(newStatus) {
+        this.status = newStatus;
+        console.log(`The order for customer ${this.customerId} is now ${this.status}.`);
+    };
+}
 
 
 /*
@@ -45,7 +57,9 @@
 */
 
 //CODE HERE
+const firstTicket = new Ticket('pizza, bread, soda', '7:03 PM', 575);
 
+//console.log(firstTicket);
 
 /*
     Call the `updateStatus` method on
@@ -54,3 +68,5 @@
 */
 
 //CODE HERE
+firstTicket.updateStatus('cooking');
+//console.log(firstTicket);
